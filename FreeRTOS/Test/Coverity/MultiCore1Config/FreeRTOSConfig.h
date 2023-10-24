f52e7841516e65182aadab0a47296df9834be94c
Ubuntu <ubuntu@ip-172-31-34-245.ap-northeast-1.compute.internal>
2023-10-24 09:33:34 +0000

    Fix test project for rule 10.4

--- FreeRTOS/Test/Coverity/MultiCore1Config/FreeRTOSConfig.h
+++ FreeRTOS/Test/Coverity/MultiCore1Config/FreeRTOSConfig.h
@@ -70,7 +70,7 @@
 #define configTICK_RATE_HZ                         ( 1000U )
 #define configMAX_PRIORITIES                       ( 7U )
 #define configMINIMAL_STACK_SIZE                   ( ( configSTACK_DEPTH_TYPE ) 70U )
-#define configMAX_TASK_NAME_LEN                    ( 5 )
+#define configMAX_TASK_NAME_LEN                    ( 5U )
 #define configUSE_16_BIT_TICKS                     0
 #define configIDLE_SHOULD_YIELD                    1
 #define configUSE_TASK_NOTIFICATIONS               1
@@ -110,7 +110,7 @@
 #define configUSE_TIMERS                           1
 #define configTIMER_TASK_PRIORITY                  ( 6U )
 #define configTIMER_QUEUE_LENGTH                   20
-#define configTIMER_TASK_STACK_DEPTH               ( configMINIMAL_STACK_SIZE * 2 )
+#define configTIMER_TASK_STACK_DEPTH               ( configMINIMAL_STACK_SIZE * 2U )
 
 /* Interrupt nesting behaviour configuration. */
 #define configKERNEL_INTERRUPT_PRIORITY            ( configMAX_PRIORITIES - 1U )
